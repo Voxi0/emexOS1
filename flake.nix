8cc574967e08fa08547923db99d5d5e670ea58c9
Voxi0 <alif200099@gmail.com>
2025-11-19 23:50:06 +0000

    chore: updated flake

--- flake.nix
+++ flake.nix
@@ -4,16 +4,18 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem(system: let
-    pkgs = import inputs.nixpkgs {inherit system;};
-  in {
-    devShells.default = pkgs.mkShellNoCC {
-      nativeBuildInputs = with pkgs; [
-        pkgsCross.x86_64-embedded.stdenv.cc
-        nasm
-        libisoburn
-        git
-      ];
-    };
-  });
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import inputs.nixpkgs {inherit system;};
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        nativeBuildInputs = with pkgs; [
+          pkgsCross.x86_64-embedded.gcc
+          qemu
+          nasm
+          libisoburn
+          git
+        ];
+      };
+    });
 }
